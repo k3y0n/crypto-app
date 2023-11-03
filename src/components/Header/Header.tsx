4bd5ec504e8bc1c54d5b80fd0258a1fb5c95022d
k3y0n <y3.0n@yandex.by>
2023-11-03 16:06:31 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,12 @@ import { HeaderProps } from "./HeaderProps";
 import styles from "./Header.module.scss";
 import TopCoins from "../TopCoins/TopCoins";
 import PortfolioInfo from "../PortfolioInfo/PortfolioInfo";
-import { useVisibleStorage } from "../../hooks/useVisibleStorage";
+//import { useVisibleStorage } from "../../hooks/useVisibleStorage";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 const Header = ({ handleSearch, search }: HeaderProps) => {
   // const [isVisible, _] = useVisibleStorage("portfolio", false);
-  const [portfolio, setPortfolio] = useLocalStorage([], "portfolio");
+  const [portfolio, _] = useLocalStorage([], "portfolio");
   console.log("header render");
 
   return (
